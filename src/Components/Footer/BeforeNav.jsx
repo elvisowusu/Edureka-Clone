27acc5b0942424c8f42e1e0f046dc9b012c64fa4
elvisowusu <owusuelvisgyasi@gmail.com>
2024-01-15 13:01:16 +0000

    Refactor BeforeNav component to include a countdown timer

--- src/Components/Footer/BeforeNav.jsx
+++ src/Components/Footer/BeforeNav.jsx
@@ -1,8 +1,28 @@
 
+
+
+
 function BeforeNav() {
+    const Timer =[{dayTime:"00",name:"D "},
+                  {dayTime:"00",name:"H "},
+                  {dayTime:"00",name:"M "},
+                  {dayTime:"00",name:"S "},
+                 ]
     return (
-        <div>
-            BEFORE NAVBAR
+        <div className="relative flex flex-col justify-center items-center ">
+            <button className="absolute right-0 top-0">close</button>
+            <section>Makar Sankranti Offer</section>
+            <section>- Flat 10% Off + 20% Cashback + Self Paced</section>
+            <section>Course Free</section>
+            <section><span>|</span> OFFER ENDING IN : </section>
+            <section>
+                {Timer.map((items,key)=>{
+                    return(
+                        <span key={key}>{items.dayTime} {items.name} </span>
+                    )
+                })}
+            </section>
+            
         </div>
     );
 }
