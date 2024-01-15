1d87ed20e66301f03b82a75b47da0809a354c47d
elvisowusu <owusuelvisgyasi@gmail.com>
2024-01-15 16:46:41 +0000

    Add search functionality to Navbar component

--- src/Components/Footer/Navbar.jsx
+++ src/Components/Footer/Navbar.jsx
@@ -1,14 +1,18 @@
 import { TfiMenu } from "react-icons/tfi";
+import { BiSearch } from "react-icons/bi";
 
 function Navbar() {
     return (
-        <div className="bg-red-200">
+        <div className="bg-red-200 px-[1rem]">
             <section className="flex justify-between items-center" >
-            <section className="inline-flex items-center">
-            <TfiMenu/>
-            <img className="w-[6rem]" src="https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1702561279/img/Edureka_V_W_logo.webp" alt="edurekaIcon"/>
+                <section className="inline-flex items-center">
+                    <TfiMenu/>
+                    <img className="w-[6rem]" src="https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1702561279/img/Edureka_V_W_logo.webp" alt="edurekaIcon"/>
+                </section>
+                <button>View Offer</button>
             </section>
-            <button>View Offer</button>
+            <section className="flex items-center justify-center">
+                <input className="w-[20rem] px-[0.9rem] py-[0.2rem]" type="search" name="" placeholder="Enter Course, Category or keyword" id="" /><BiSearch/>
             </section>
         </div>
     );
