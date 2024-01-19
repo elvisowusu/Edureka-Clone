847b6f2e32d29e4b412db183d965df1797a1bcd0
elvisowusu <owusuelvisgyasi@gmail.com>
2024-01-19 15:56:59 +0000

    Advancing

--- src/Components/Footer/BeforeNav.jsx
+++ src/Components/Footer/BeforeNav.jsx
@@ -1,7 +1,7 @@
 import { MdClose } from "react-icons/md";
 import { useState } from "react";
 
-function BeforeNav() {
+function BeforeNav({onCloseClicked}) {
     const [clickClose, setclickClose] = useState(false)
 
     const Timer =[{dayTime:"00",name:"D "},
@@ -9,13 +9,17 @@ function BeforeNav() {
                   {dayTime:"00",name:"M "},
                   {dayTime:"00",name:"S "},
                  ]
+    const handleClicks = () => {
+        setclickClose(!clickClose)
+        onCloseClicked();
+    }
     return (
         <>
         { clickClose?
             null
         :
         <div className="relative flex flex-col justify-center items-center ">
-            <button onClick={()=>{setclickClose(!clickClose)}} className="absolute right-0 top-0"><MdClose /></button>
+            <button onClick={handleClicks} className="absolute right-0 top-0"><MdClose /></button>
             <section>Makar Sankranti Offer</section>
             <section>- Flat 10% Off + 20% Cashback + Self Paced</section>
             <section>Course Free</section>
--- src/Components/Footer/Navbar.jsx
+++ src/Components/Footer/Navbar.jsx
@@ -1,29 +1,53 @@
 import { TfiMenu } from "react-icons/tfi";
-import { BiSearch } from "react-icons/bi";
+import { BiSearch, BiSolidPhone } from "react-icons/bi";
 import { useState } from "react";
 import BeforeNav from "./BeforeNav";
 
 function Navbar() {
-    const [click,setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [closeClicked, setCloseClicked] = useState(false);
+    const [viewOfferClicked, setViewOfferClicked] = useState(false);
 
-    const handleClick = () => setClick(!click)
+    const handleClick = () => {
+        setClick(!click);
+        setCloseClicked(false);
+        setViewOfferClicked(true);
+    };
+
+    const handleCloseClicked = () => {
+        setCloseClicked(true);
+        setViewOfferClicked(false);
+    };
+
     return (
         <div>
-            {click?null:<BeforeNav/>}
+            {click || viewOfferClicked ? null : <BeforeNav onCloseClicked={handleCloseClicked} />}
             <div className="bg-red-200 px-[1rem]">
-            <section className="flex justify-between items-center" >
-                <section className="inline-flex items-center">
-                    <TfiMenu/>
-                    <img className="w-[6rem]" src="https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1702561279/img/Edureka_V_W_logo.webp" alt="edurekaIcon"/>
+                <section className="flex justify-between items-center">
+                    <section className="inline-flex items-center">
+                        <TfiMenu />
+                        <img
+                            className="w-[6rem]"
+                            src="https://d1jnx9ba8s6j9r.cloudfront.net/imgver.1702561279/img/Edureka_V_W_logo.webp"
+                            alt="edurekaIcon"
+                        />
+                    </section>
+                    <section className="inline-flex items-center gap-1">
+                        <BiSolidPhone className="text-white" /> {closeClicked && !viewOfferClicked && <button onClick={handleClick}>View Offer</button>}
+                    </section>
                 </section>
-                <button onClick={handleClick}>View Offer</button>
-            </section>
-            <section className="flex items-center justify-center">
-                <input className="w-[20rem] px-[0.9rem] py-[0.2rem]" type="search" name="" placeholder="Enter Course, Category or keyword" id="" /><BiSearch/>
-            </section>
+                <section className="flex items-center justify-center">
+                    <input
+                        className="w-[20rem] px-[0.9rem] py-[0.2rem]"
+                        type="search"
+                        name=""
+                        placeholder="Enter Course, Category or keyword"
+                        id=""
+                    />
+                    <BiSearch />
+                </section>
+            </div>
         </div>
-        </div>
-        
     );
 }
 
