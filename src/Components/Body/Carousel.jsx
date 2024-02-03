4b4cb5080409fc89f363766a119dfb9b36942889
elvisowusu <owusuelvisgyasi@gmail.com>
2024-02-03 19:27:28 +0000

    Add new dependencies: react-carousel, react-responsive-carousel, debounce, range

--- src/Components/Body/Carousel.jsx
+++ src/Components/Body/Carousel.jsx
@@ -1,7 +1,21 @@
-export default function Carousel({children: slides}){
-    return(
-        <div>
-            <div>{slides}</div>
-        </div>
-    )
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+function Carousel() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+    return (
+        <Slider {...settings}>
+                <img src="https://d24vv731hdkcnd.cloudfront.net/carousel-banner/1706687183-Web-banner_1920x410.jpg" alt="" />
+
+                <img src="https://d24vv731hdkcnd.cloudfront.net/carousel-banner/1694011067-Current-Home-Page-Banner_1920x410.jpg" alt="" />
+        </Slider>
+    );
 }
+
+export default Carousel;
