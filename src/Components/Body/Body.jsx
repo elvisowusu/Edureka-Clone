b2afcbbe4f829dce284ca6304caa5686e51b6a9f
elvisowusu <owusuelvisgyasi@gmail.com>
2024-02-03 20:03:22 +0000

    Refactor Body component to include trending courses section

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -3,7 +3,7 @@ function Body() {
     const slides =["https://d24vv731hdkcnd.cloudfront.net/carousel-banner/1706687183-Web-banner_1920x410.jpg","https://d24vv731hdkcnd.cloudfront.net/carousel-banner/1694011067-Current-Home-Page-Banner_1920x410.jpg"]
 
     return (
-        <div>
+        <section className="relative">
            <section className="max-w-lg">
                <Carousel>
                      {slides.map((slide, index) => (
@@ -11,7 +11,14 @@ function Body() {
                      ))}
                 </Carousel>
            </section>
-        </div>
+           <section>
+             <h1 className="">Trending Courses</h1>
+             <section className="flex justify-between px-[1rem]">
+                <h1 className="">All</h1>
+                <button>Menu</button>
+             </section>
+           </section>
+        </section>
     );
 }
 
