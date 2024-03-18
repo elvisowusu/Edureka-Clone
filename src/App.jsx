ad7c599ea74fc580ec4044affa45f98a9e40f60d
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-18 11:59:43 +0000

    Add NavMenu component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Body from "./Components/Body/Body"
+import NavMenu from "./Components/NewFooter/NavMenu"
 import NewFooter from "./Components/NewFooter/newFooter"
 
 function App() {
@@ -6,8 +7,9 @@ function App() {
   return (
     <div className="font-openSans">
      {/* <Footer/> */}
-     <NewFooter/>
-     <Body/>
+     {/* <NewFooter/>
+     <Body/> */}
+     <NavMenu/>
     </div>
   )
 }
---
+++ src/Components/NewFooter/NavMenu.jsx
@@ -0,0 +1,11 @@
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+function NavMenu() {
+    return (
+        <div className="w-[20rem]">
+            <section className="flex items-center justify-between bg-[#f4f4f4] text-[#4a4a4a] font-semibold text-[15px] px-[30px] pt-[20px] pb-[19px] border-b-[#d7d7d7] border-b-2">Browse Categories <IoIosCloseCircleOutline size={23} className="text-[#4a4a4a]"/></section>
+        </div>
+    );
+}
+
+export default NavMenu;
