551df1d7fd52c8448bd6ef7f631d50b964acb7b1
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-20 17:17:55 +0000

    Refactor header navigation menu and categories items

--- src/Components/Header/Categories/CategoriesItems.jsx
+++ src/Components/Header/Categories/CategoriesItems.jsx
@@ -1,11 +1,11 @@
 import { IoIosArrowForward } from "react-icons/io";
 
-function CategoriesItems() {
+function CategoriesItems({CategoriesItemsFunction}) {
     const categories = ['Cloud Computing','DevOps','Cyber Security','BI and Visualization','Programming & Frameworks','Data Science','Project Management and Methodologies','PG Programs','Artificial Intelligence','Big Data','Software Testing','Frontend Development','Databases','Robotic Process Automation','Data Warehousing and ETL','Mobile Development','Operating Systems','Architecture & Design Patterns','Blockchain','Digital Marketing']
     return (
-        <div>
+        <div className="overflow-scroll">
             {categories.map((category, index) => (
-                <button key={index} className="py-[14px] block">{category} <IoIosArrowForward/> </button>
+                <button key={index} className="w-full py-[14px] flex justify-between items-center"><span className="text-left">{category}</span> <IoIosArrowForward size={21}/> </button>
             ))}
         </div>
     );
--- src/Components/Header/NavMenu.jsx
+++ src/Components/Header/NavMenu.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
+import CategoriesItems from "./Categories/CategoriesItems";
 function NavMenu({NavMunuFunction}) {
+    const [categories,SetCategories]=useState(false)
+    const categoriesItems=()=>{
+        SetCategories(!categories)
+    }
     return (
         <div className="fixed top-0 z-50 w-[83%]">
             <section onClick={NavMunuFunction} className="fixed w-full -z-10 h-full opacity-65 bg-[rgba(0,0,0,0.5)]"></section>
             <button onClick={NavMunuFunction}  className="flex items-center w-full justify-between bg-[#f4f4f4] text-[#4a4a4a] font-semibold text-[15px] px-[30px] pt-[20px] pb-[19px] border-b-[#d7d7d7] border-b-2">Browse Categories <IoIosCloseCircleOutline size={23} className="text-[#4a4a4a]"/></button>
             <section className="px-[30px] text-[#4a4a4a] bg-white h-[60rem]">
-                    <button className="flex py-[14px] w-full justify-between items-center">Categories<IoIosArrowForward size={21} className={``}/> </button>
+                
+                {categories?
+                    <CategoriesItems categoriesFunction={CategoriesItems}/>
+                    :
+                <>
+                    <button onClick={categoriesItems} className="flex py-[14px] w-full justify-between items-center">Categories<IoIosArrowForward size={21}/> </button>
                     <button className="py-[14px] block">Corporate Training </button>
                     <button className="py-[14px] ">Courses </button>
-                <button className="items-center w-full py-[12px] text-[#404040] font-bold text-[14px] border border-[#e9963e] my-[15px]">Log In</button>
-                <button className="items-center w-full py-[12px] text-white font-bold text-[14px] bg-[#e9963e] mb-[15px]">Sign Up</button>
+                    <button className="items-center w-full py-[12px] text-[#404040] font-bold text-[14px] border border-[#e9963e] my-[15px]">Log In</button>
+                    <button className="items-center w-full py-[12px] text-white font-bold text-[14px] bg-[#e9963e] mb-[15px]">Sign Up</button>
+                </>
+                }
             </section>
         </div>
     );
