db79dcd8729b0ef0780cc01bb561332cc7401f0f
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-21 18:19:37 +0000

    more features o navMenu

--- src/Components/Header/NavMenu.jsx
+++ src/Components/Header/NavMenu.jsx
@@ -11,7 +11,7 @@ function NavMenu({NavMunuFunction}) {
         <div className="fixed top-0 z-50 w-[83%]">
             <section onClick={NavMunuFunction} className="fixed w-full -z-10 h-full opacity-65 bg-[rgba(0,0,0,0.5)]"></section>
             <button onClick={NavMunuFunction}  className="flex items-center w-full justify-between bg-[#f4f4f4] text-[#4a4a4a] font-semibold text-[15px] px-[30px] pt-[20px] pb-[19px] border-b-[#d7d7d7] border-b-2">Browse Categories <IoIosCloseCircleOutline size={23} className="text-[#4a4a4a]"/></button>
-            <section className="px-[30px] text-[#4a4a4a] bg-white h-[60rem] overflow-scroll">
+            <section className="px-[30px] text-[#4a4a4a] bg-white">
                 
                 {categories?
                     <CategoriesItems CategoriesState={categories} CategoriesFunction={categoriesItemsFunction}/>
