b47173fab010d3875a2eaf439959f8a66f5ad5a8
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-21 00:24:35 +0000

    more features

--- src/Components/Header/Categories/CategoriesItems.jsx
+++ src/Components/Header/Categories/CategoriesItems.jsx
@@ -1,10 +1,10 @@
-import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-function CategoriesItems({CategoriesItemsFunction}) {
+function CategoriesItems({CategoriesFunction}) {
     const categories = ['Cloud Computing','DevOps','Cyber Security','BI and Visualization','Programming & Frameworks','Data Science','Project Management and Methodologies','PG Programs','Artificial Intelligence','Big Data','Software Testing','Frontend Development','Databases','Robotic Process Automation','Data Warehousing and ETL','Mobile Development','Operating Systems','Architecture & Design Patterns','Blockchain','Digital Marketing']
     return (
         <div className="overflow-scroll">
-            <button onClick={CategoriesItemsFunction} className="flex py-[14px] w-full items-center"><IoIosArrowForward size={21}/> Categories</button>
+            <button onClick={CategoriesFunction} className="flex py-[14px] w-full items-center "><IoIosArrowBack size={21}/> Categories</button>
             {categories.map((category, index) =>(
                 <button key={index} className="w-full py-[14px] flex justify-between items-center"><span className="text-left">{category}</span> <IoIosArrowForward size={21}/> </button>
             ))}
