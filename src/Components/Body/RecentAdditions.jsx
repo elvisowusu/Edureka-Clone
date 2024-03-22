77efe11d2e2c2cd077e22db2425e67c76b1a93a3
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-22 10:08:58 +0000

    Add useEffect and AOS library to NavMenu component

---
+++ src/Components/Body/RecentAdditions.jsx
@@ -0,0 +1,60 @@
+import{ useState } from 'react';
+import { FaStar } from "react-icons/fa6";
+
+
+
+function RecentAdditions() {
+    const content =[{courseName:'Advanced Certificate Program in Data Science',bgGradient:"bg-[linear-gradient(240deg,#426a9f,#4edbec)]",img:'',number:"",stars:""},
+                    {courseName:'Artificial Intelligence Certification Courses',bgGradient:"bg-pink",img:"",number:"",stars:""},
+                    {courseName:'Advanced Certificate Program in cloud Computing',bgGradient:"bg-pink",img:'',number:"",stars:""},
+                    {courseName:'Microsoft SQL Course',bgGradient:"bg-pink",img:"",number:"",stars:""},
+                    {courseName:'UI UX Design Certification Course',bgGradient:"bg-pink",img:"",number:"",stars:""},
+                    {courseName:'CISSP Certification Training Course',bgGradient:"bg-pink",img:"",number:"",stars:""},
+                    {courseName:'Flutter Application Development Course',bgGradient:"bg-pink",img:'',number:"",stars:""},
+                    {courseName:'Human-Computer Interaction(HCI) for AI Systems Design',bgGradient:"bg-pink",img:"",number:"",stars:""},
+    ]
+    // State to manage ratings for each course
+    const [ratings, setRatings] = useState(new Array(content.length).fill(0));
+    // Function to handle rating click event
+    const handleRatingClick = (index, rating) => {
+        // Clone the existing ratings array
+        const newRatings = [...ratings];
+        // Update the rating for the clicked course
+        newRatings[index] = rating;
+        // Update the state with new ratings
+        setRatings(newRatings);
+      };
+    return (
+        <div>
+            <h1 className='px-[0.6rem] my-[1.5rem] text-[#404040] font-semibold'>Career Related Programs</h1>
+            <section className="px-[1.5rem]">
+                {content.map((items,key)=>{
+                return <section key={key} className="flex my-[1.3rem]">
+                <div className={`${items.bgGradient} text-[50px] h-[100px] w-[93px] min-w-[93px] flex items-center justify-center`}>
+                    {items.img}
+                </div>
+                <div className="px-[10px] py-[11px] flex flex-col justify-between">
+                    <h1>{items.courseName}</h1>
+                    <div className="flex items-center gap-[0.2rem]">
+                    {[...Array(5)].map((_, starIndex) => (
+                  <span
+                    key={starIndex}
+                    onClick={() => handleRatingClick(key, starIndex + 1)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    {/* Render filled star if starIndex is less than the current rating, otherwise render outline star */}
+                    {starIndex < ratings[key] ? <FaStar className="inline text-[#ffc700]" /> : <FaStar className="inline text-[#f0f0f0]" />}
+                  </span>
+                ))}
+                {/* Display the number of ratings */}
+                <span className="font-semibold pt-1 pl-1">{ratings[key]}</span>
+                    </div>
+                </div>
+            </section>
+            })}
+            </section>
+        </div>
+    );
+}
+
+export default RecentAdditions;
--- src/Components/Header/NavMenu.jsx
+++ src/Components/Header/NavMenu.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import 'aos/dist/aos.css'
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import CategoriesItems from "./Categories/CategoriesItems";
@@ -7,11 +9,18 @@ function NavMenu({NavMunuFunction}) {
     const categoriesItemsFunction=()=>{
         SetCategories(!categories)
     }
+    useEffect(()=>{
+        AOS.init();
+        AOS.refresh();
+        return ()=>{
+            AOS.refresh()
+        }
+    },[])
     return (
-        <div className="fixed top-0 z-50 w-[83%]">
-            <section onClick={NavMunuFunction} className="fixed w-full -z-10 h-full opacity-65 bg-[rgba(0,0,0,0.5)]"></section>
+        <div data-aos={`${categories?'fade-down':'fade-right'}`} className="fixed h-full top-0 z-50 w-[83%]">
+            <section onClick={NavMunuFunction} className="fixed w-[rem] -z-10 h-full  opacity-65 bg-[rgba(0,0,0,0.5)]"></section>
             <button onClick={NavMunuFunction}  className="flex items-center w-full justify-between bg-[#f4f4f4] text-[#4a4a4a] font-semibold text-[15px] px-[30px] pt-[20px] pb-[19px] border-b-[#d7d7d7] border-b-2">Browse Categories <IoIosCloseCircleOutline size={23} className="text-[#4a4a4a]"/></button>
-            <section className="px-[30px] text-[#4a4a4a] bg-white">
+            <section className="px-[30px] text-[#4a4a4a] bg-white h-full">
                 
                 {categories?
                     <CategoriesItems CategoriesState={categories} CategoriesFunction={categoriesItemsFunction}/>
@@ -22,7 +31,7 @@ function NavMenu({NavMunuFunction}) {
                     <button className="py-[14px] ">Courses </button>
                     <button className="items-center w-full py-[12px] text-[#404040] font-bold text-[14px] border border-[#e9963e] my-[15px]">Log In</button>
                     <button className="items-center w-full py-[12px] text-white font-bold text-[14px] bg-[#e9963e] mb-[15px]">Sign Up</button>
-                </>
+                </> 
                 }
             </section>
         </div>
