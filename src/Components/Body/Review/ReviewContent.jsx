8223db5bcf9cc441604c011cb10926ebd64c0e81
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-22 23:27:40 +0000

    Refactor ReviewContent component

--- src/Components/Body/Review/ReviewContent.jsx
+++ src/Components/Body/Review/ReviewContent.jsx
@@ -1,11 +1,4 @@
 
-
-function ReviewContent() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
-
-export default ReviewContent;
+const content =[
+    {img:'',Name:'Vivek Yadav',Profession:'Customer Project Manager',Course:'AI and Machine Learning Masters Course',Rating:'',passage:'The Postgraduate Diploma in AIby Edureka in collaboration with NIT Warangal offered a superb learning experience for working professionals with convenient weekend live classes.'},
+];
