14dee152161bfc4486a6a305b5fe9be7cc7d4025
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-22 23:08:12 +0000

    Fix bug in login functionality

---
+++ src/Components/Body/Reviews.jsx
@@ -0,0 +1,24 @@
+import { RxLinkedinLogo } from "react-icons/rx";
+
+function Reviews() {
+    return (
+        <div className="flex col px-[30px] py-[25px] mb-[15px] h-[250px]">
+            <section>
+                <img src="" alt="" />
+                <section>
+                    <p><span>Name</span> <RxLinkedinLogo /></p>
+                    <p>Profession</p>
+                </section>
+            </section>
+            <section>
+                <p>Course</p>
+                <p>Rating</p>
+            </section> 
+            <p>
+                content
+            </p>
+        </div>
+    );
+}
+
+export default Reviews;
