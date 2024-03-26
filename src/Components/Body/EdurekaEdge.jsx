89b7c9b2c14d993bf1826c0f9ecf47ac94f2071b
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-26 13:55:59 +0000

    Add content to EdurekaEdge component

--- src/Components/Body/EdurekaEdge.jsx
+++ src/Components/Body/EdurekaEdge.jsx
@@ -1,12 +1,24 @@
 
 function EdurekaEdge() {
+    const Content=[{icon:'',h2:'Real Time Doubt Resolution',p:'with Live Instructors'},
+                   {icon:'',h2:'Unlimited Access',p:'to Online Learning Repository'},
+                   {icon:'',h2:'Learning Assistants',p:'to Guide When You Are Stuck'},
+                   {icon:'',h2:'Track Your',p:'Career Growth'}
+        ]
     return (
         <div className="mx-[0.6rem]">
             <p className="mt-[15px] font-semibold">The Edureka Edge</p>
-            <div className="">
-                <section className="h-[85px]">
-                    <img src="" alt="" />
-                </section>
+            <div className="mt-[15px]">
+                <div>
+                    <section className="h-[85px]">
+                        <img src="" alt="" />
+                    </section>
+                    <section>
+                        <h2>
+
+                        </h2>
+                    </section>
+                </div>
             </div>
         </div>
     );
