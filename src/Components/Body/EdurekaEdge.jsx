6db81dbee4bc9a57f1f4f4a965423654f5b95dd9
elvisowusu <owusuelvisgyasi@gmail.com>
2024-03-26 15:40:27 +0000

    Add useEffect hook and update animation duration

--- src/Components/Body/EdurekaEdge.jsx
+++ src/Components/Body/EdurekaEdge.jsx
@@ -1,7 +1,7 @@
+import { useEffect } from "react";
 import { IoCheckmarkSharp } from "react-icons/io5";
 import { FaInfinity } from "react-icons/fa";
 import { BsGraphUpArrow } from "react-icons/bs";
-import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 function EdurekaEdge() {
@@ -10,11 +10,12 @@ function EdurekaEdge() {
                    {icon:<p className="text-[80px] bg-[linear-gradient(108deg,#248de4,#243be4)] inline bg-clip-text text-transparent">24/7</p>,h2:'Learning Assistants',p:'to Guide When You Are Stuck'},
                    {icon:<BsGraphUpArrow size={60} className="text-orange-500"/>,h2:'Track Your',p:'Career Growth'}
         ]
+       
     return (
         <div className="mx-[0.6rem]">
             <p className="my-[15px] font-semibold">The Edureka Edge</p>
             {Content.map((content,index)=>{
-                return <div key={index} className="pt-[15px] flex justify-center">
+                return <div data-aos="fade-up" data-aos-duration="700"  key={index} className="pt-[15px] flex justify-center">
                         <div className=" w-[200px]">
                             <section className="h-[85px] flex justify-center items-center ">
                                 {content.icon}
